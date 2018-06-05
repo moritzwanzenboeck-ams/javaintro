f79ba0fdb5568bfd770c07ea805ed53982e58343
moritzwanzenboeck <moritzwanzenboeck@AMS-GYM-EDV1-04.gym.ams.local>
2018-06-05 15:46:30 +0200

    MW

---
+++ MyFirstGUIProject/src/dbtest/DBtest.java
@@ -0,0 +1,25 @@
+package dbtest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBtest {
+
+		public static void main(String[]args) {
+			try {
+				new DBtest();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		public DBtest() throws SQLException {
+			Connection conn = DriverManager.getConnection("jdbc:ucanaccess://C:/Users/moritzwanzenboeck/Documents/dbtest1.accdb;memory=false");
+			
+			Statement stmt = conn.createStatement();
+			stmt.execute("INSERT INTO adresse (vorname, nachname, ort, plz)" + "VALUES ('Clemens','Thalhammer','Wien',1190)");
+		}
+}
